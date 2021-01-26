9f6eb7fd8baeb443015a166c48ceaf34071cfd7f
Austin Takam <austin.takam@ibm.com>
2021-01-26 01:09:34 -0500

    new client

--- client/src/index.js
+++ client/src/index.js
@@ -1,39 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
 
-import combineReducers from "./reducers";
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
 
-import "assets/css/material-dashboard-react.css?v=1.4.1";
-import "assets/css/custom.css";
-
-import indexRoutes from "routes/index.jsx";
-
-import { fetchAuthenticated } from "./actions/account";
-
-const hist = createBrowserHistory();
-
-const store = createStore(
-  combineReducers,
-  applyMiddleware(thunk)
-);
-
-store.dispatch(fetchAuthenticated())
-  .then(() => {
-    ReactDOM.render(
-      <Provider store={store}>
-        <Router history={hist}>
-          <Switch>
-            {indexRoutes.map((prop, key) => {
-              return <Route path={prop.path} component={prop.component} key={key} />;
-            })}
-          </Switch>
-        </Router>
-      </Provider>,
-      document.getElementById("root")
-    );
-  });
+serviceWorker.unregister();
